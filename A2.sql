1f2263d103904a7d4cd59636dfef0a2f8e973719
rmc3408 <rmc3408@protonmail.com>
2020-07-05 19:23:32 -0400

    a2

--- A2.sql
+++ A2.sql
@@ -51,54 +51,58 @@ SELECT * FROM dd_project;
 -- Question 3-11
 
 DECLARE 
-PLEDGES DD_PLEDGE%ROWTYPE;
-s_Date DD_PLEDGE.PLEDGEDATE%TYPE := '01-OCT-12';
-e_Date DD_PLEDGE.PLEDGEDATE%TYPE := '31-OCT-12';
-
-BEGIN
-FOR PLEDGES IN
-    (SELECT IDPLEDGE, IDDONOR, PLEDGEAMT, 
-
-CASE 
-  WHEN PAYMONTHS = 0 THEN 'Lump Sum.'
-  ELSE 'Monthly - ' || PAYMONTHS END AS Monthly
-  
-    FROM DD_PLEDGE
-    WHERE PLEDGEDATE >= s_Date AND PLEDGEDATE <= e_Date
-    ORDER BY PAYMONTHS)
-
-LOOP 
-DBMS_OUTPUT.PUT_LINE('Pledge ID: ' || PLEDGES.IDPLEDGE 
-|| ', Donor ID: '|| PLEDGES.IDDONOR 
-|| ', Pledge Amount: ' ||TO_CHAR(PLEDGES.PLEDGEAMT,'$999999.99') 
-|| ', Monthly Payments: ' || PLEDGES.Monthly);
-END LOOP;
+    TYPE pledge_type IS TABLE OF dd_pledge%ROWTYPE 
+        INDEX BY PLS_INTEGER; 
+    table_pledge pledge_type;
+    payment VARCHAR2(12);
+BEGIN 
+    SELECT * BULK COLLECT 
+        INTO table_pledge 
+        FROM dd_pledge
+        WHERE idpledge IN(
+            SELECT idpledge
+            FROM dd_pledge
+            WHERE EXTRACT(MONTH FROM TO_DATE(pledgedate, 'DD-MON-YYYY')) = '10')
+        ORDER BY paymonths;
+    FOR x IN 1.. table_pledge.COUNT LOOP
+        IF table_pledge (x).paymonths = 0 THEN
+            payment := 'Lump Sum';
+        ELSE
+            payment := 'Monthly - '|| table_pledge (x).paymonths;
+        END IF;
+        DBMS_OUTPUT.PUT_LINE(
+        'Pledge Date: ' ||table_pledge (x).pledgedate || 
+        ' , Pledge ID: ' || table_pledge (x).idpledge || 
+        ' , Pledge Amount: ' || table_pledge (x).pledgeamt || 
+        ' , PayMent ' || payment);
+    END LOOP; 
 END;
-
 
 
 
 -- Question 3-12
 
 DECLARE
-PLEDGES DD_PLEDGE%ROWTYPE;
-BALANCE DD_PLEDGE.PLEDGEAMT%TYPE;
-MONTHS_TOTAL DD_PLEDGE.PAYMONTHS%TYPE:= 24;
-PLEDGE_MONTHLY DD_PLEDGE.PLEDGEAMT%TYPE;
+    p dd_pledge%ROWTYPE;
+    amt dd_pledge.pledgeamt%TYPE;
+    Balance dd_pledge.pledgeamt%TYPE;
+    id dd_pledge.idpledge%TYPE := 104;
 BEGIN
-  SELECT * INTO PLEDGES
-  FROM DD_PLEDGE
-  WHERE IDPLEDGE = 105;
-  IF PLEDGES.PAYMONTHS = 0 THEN
-  BALANCE := 0;
-  ELSE BALANCE := (MONTHS_TOTAL-PLEDGES.PAYMONTHS)*(PLEDGES.PLEDGEAMT/PLEDGES.PAYMONTHS);
-  END IF;
-    DBMS_OUTPUT.PUT_LINE('Pledge ID: ' || PLEDGES.IDPLEDGE 
-    || ', Donor ID: ' || PLEDGES.IDDONOR 
-    || ', Pledge amount: ' || PLEDGES.PLEDGEAMT 
-    || ', Balance: ' || BALANCE);
+    SELECT *
+    INTO p
+    FROM dd_pledge
+    WHERE idpledge = id;
+    SELECT sum(payamt)
+    INTO amt
+    FROM dd_payment
+    WHERE idpledge =id;
+    BALANCE := p.pledgeamt - amt;
+    DBMS_OUTPUT.PUT_LINE('Pledge ID: ' || p.idpledge || 
+        ' Donor ID: ' || p.iddonor || 
+        ' Pledge amount: '|| p.pledgeamt || 
+        ' Total: ' || amt || 
+        ' Balance: ' || Balance);
 END;
-
 
 
 
@@ -109,20 +113,16 @@ DECLARE
   p_name dd_project.projname%TYPE;
   d_start dd_project.projstartdate%TYPE;
   f_goal dd_project.projfundgoal%TYPE;  
-  new_Fgoal	dd_project.projfundgoal%TYPE := 150000;
-
-  p_id dd_project.idproj%TYPE := 502;
-
+  new_Fgoal	dd_project.projfundgoal%TYPE := 22000;
+  p_id dd_project.idproj%TYPE := 501;
 BEGIN
   SELECT projname, projstartdate, projfundgoal
   INTO p_name, d_start, f_goal
   FROM DD_PROJECT
   WHERE idproj = p_id; 
-
   UPDATE dd_project
   SET projfundgoal = new_Fgoal
   WHERE idproj = p_id;  
-  
 DBMS_OUTPUT.PUT_LINE('Project Name: ' || p_name);
 DBMS_OUTPUT.PUT_LINE('Start Date: ' || d_start );
 DBMS_OUTPUT.PUT_LINE('Previous Goal: ' || f_goal);
