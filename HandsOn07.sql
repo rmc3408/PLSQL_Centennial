23d693177b48536a86dc39777b28648971916c55
rmc3408 <rmc3408@protonmail.com>
2020-07-15 17:15:49 -0400

    a

--- HandsOn07.sql
+++ HandsOn07.sql
@@ -4,7 +4,10 @@ PROCEDURE stkup_pp
 p_qty IN NUMBER);
 END;
 
+
+
 CREATE OR REPLACE PACKAGE BODY prod_pkg IS
+
 FUNCTION stk_pf
 (p_id IN bb_product.idproduct%TYPE)
 RETURN NUMBER
@@ -56,3 +59,67 @@ END;
 OR
 
 EXECUTE prod_pkg.stkup_pp(1,3);
+
+
+
+----------------------------------------------------------
+
+CREATE OR REPLACE PACKAGE order_info_pkg
+IS
+FUNCTION ship_name_pf
+(p_basket IN NUMBER)
+RETURN VARCHAR2;
+PROCEDURE basket_info_pp
+(p_basket IN NUMBER,
+p_shop OUT NUMBER,
+p_date OUT DATE);
+END;
+
+CREATE OR REPLACE PACKAGE BODY order_info_pkg
+IS
+FUNCTION ship_name_pf
+(p_basket IN NUMBER)
+RETURN VARCHAR2
+IS
+lv_name_txt VARCHAR2(25);
+BEGIN
+SELECT shipfirstname||' '||shiplastname
+INTO lv_name_txt
+FROM bb_basket
+WHERE idBasket = p_basket;
+RETURN lv_name_txt;
+EXCEPTION
+WHEN NO_DATA_FOUND THEN
+DBMS_OUTPUT.PUT_LINE('Invalid basket id');
+END ship_name_pf;
+PROCEDURE basket_info_pp
+(p_basket IN NUMBER,
+p_shop OUT NUMBER,
+p_date OUT DATE)
+IS
+BEGIN
+SELECT idshopper, dtordered
+INTO p_shop, p_date
+FROM bb_basket
+WHERE idbasket = p_basket;
+EXCEPTION
+WHEN NO_DATA_FOUND THEN
+DBMS_OUTPUT.PUT_LINE('Invalid basket id');
+END basket_info_pp;
+END;
+
+DECLARE
+lv_name_txt VARCHAR2(20);
+lv_id_num NUMBER(4);
+lv_ord_dat DATE;
+BEGIN
+lv_name_txt := order_info_pkg.ship_name_pf(4);
+order_info_pkg.basket_info_pp(4,lv_id_num, lv_ord_dat);
+DBMS_OUTPUT.PUT_LINE(lv_name_txt);
+DBMS_OUTPUT.PUT_LINE(lv_id_num);
+DBMS_OUTPUT.PUT_LINE(lv_ord_dat);
+END;
+
+SELECT order_info_pkg.ship_name_pf(idBasket)
+FROM bb_basket
+WHERE idBasket = 12;
